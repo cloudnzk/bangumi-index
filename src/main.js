ba0aa9ad9077b5d16d960b149201ca5e839f892f
silentlight <526416486@qq.com>
2022-03-11 00:36:11 +0800

    首页导航栏布局完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 
+// 注册懒加载组件
+import { Lazyload } from 'vant';
+Vue.use(Lazyload);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>首页</div>
+  <div>
+    <van-nav-bar title="首页">
+      <template #left>
+        <van-icon name="replay" size="18" />
+      </template>
+      <template #right>
+        <van-icon name="search" size="18" />
+      </template>
+    </van-nav-bar>
+    
+    <!-- 轮播图组件 -->
+    <van-swipe :autoplay="3000" class="my-swipe">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
 </template>
 <script> 
-export default {
-  name: "Home",
-  data () {
-   return {}
-  },
-}
+  import { NavBar } from 'vant';
+  import { Icon } from 'vant';
+  import { Image as VanImage } from 'vant';
+  import { Swipe, SwipeItem } from 'vant';
+
+  export default {
+    name: "Home",
+    components: {
+      [NavBar.name]: NavBar,
+      [Icon.name]: Icon,
+      [VanImage.name]: VanImage,
+      [Swipe.name]: Swipe,
+      [SwipeItem.name]: SwipeItem,
+    },
+    data () {
+      return {
+        images: [
+          'https://img01.yzcdn.cn/vant/apple-1.jpg',
+          'https://img01.yzcdn.cn/vant/apple-2.jpg',
+        ],
+      }
+    },
+  }
 </script>
 <style scoped>
 </style>
