13ae57ebe917f145f1b7ab723153e4c8281beba7
silentlight <526416486@qq.com>
2022-03-11 20:07:15 +0800

    每日放送显示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <van-search v-model="value" shape="round" placeholder="请输入搜索关键词" />
+    <calendar :animes="animes"></calendar>
   </div>
 </template>
 <script> 
   import { NavBar } from 'vant';
   import { Icon } from 'vant';
   import { Image as VanImage } from 'vant';
-  import { Swipe, SwipeItem } from 'vant';
   import { Search } from 'vant';
 
+  import Calendar from './childComps/Calendar'
   import {getCalendar} from 'network/home'
 
   export default {
@@ -18,21 +19,30 @@
       [NavBar.name]: NavBar,
       [Icon.name]: Icon,
       [VanImage.name]: VanImage,
-      [Swipe.name]: Swipe,
-      [SwipeItem.name]: SwipeItem,
       [Search.name]: Search,
+      Calendar,
     },
     data () {
       return {
-        value: ''
+        value: '',
+        animes: [],
+        today: 0,
       }
     },
     created(){
-      getCalendar().then(res => {
-        console.log(res);
-      })
+      this.getCalendar();
     },
-    
+    methods: {
+      getCalendar(){
+        getCalendar().then(res => {
+          // console.log(res);
+          // 保存请求过来的数据到 data 中
+          // 用一个计算属性计算今天是周几
+          this.animes = res.data[this.today].items;
+          // console.log(this.animes);
+        })
+      }
+    }
   }
 </script>
 <style scoped>
---
+++ src/views/home/childComps/Calendar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <van-grid :column-num="2">
+        <van-grid-item v-for="(item,index) in animes" :key="index" icon="photo-o" :text="bangumiName(item)" />
+    </van-grid>
+  </div>
+</template>
+<script>
+  import { Grid, GridItem } from 'vant';
+
+  export default {
+    name:'',
+    mixins: [],
+    components: {
+        [Grid.name]: Grid,
+        [GridItem.name]: GridItem,
+    },
+    props:{
+      animes: {
+          type: Array,
+          default(){
+              return []
+          }
+      }
+    },
+    data () {
+      return {
+
+      };
+    },
+    computed: {
+      // 如何用闭包传值，好好理解下！
+      bangumiName(){
+        return function(item){
+          return item.name_cn.length === 0 ? item.name : item.name_cn
+        }
+      },
+    },
+    watch: {},
+    created() {},
+    mounted() {},
+    destroyed() {},
+    methods: {
+    },
+  }
+</script>
+<style scoped>
+
+</style>
